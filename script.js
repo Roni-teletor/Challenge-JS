bdf79b78b93edced516facfb0cc9809bdb6116b3
Cristian Teletor <ronicristianteletor@gmail.com>
2024-10-07 23:23:54 +0200

    did a function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-for (let i = 1; i <= 100; i++) {
+/*for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(i + " -- FIZZBUZZ");
     } else if (i % 3 === 0) {
@@ -9,4 +8,25 @@ for (let i = 1; i <= 100; i++) {
     } else {
         console.log(i + " -- " + i);
     }
+} */
+
+/*const there =  (x,y, z) => {
+ 
+    let result = x * y + z
+    return result
 }
+ 
+there(7,30, 12)
+console.log(there())*/
+
+
+
+
+const sum = (x, y, z) => {
+    let result = (x % y) + z;
+    return result; 
+}
+
+
+sum(15, 24, 56)
+console.log(sum(15, 24, 56));
