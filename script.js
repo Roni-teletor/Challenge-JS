58f28f4c72991e05e6578625a0094f5467ccd755
Cristian Teletor <ronicristianteletor@gmail.com>
2024-10-07 19:59:05 +0200

    fixed my code

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
+
 for (let i = 1; i <= 100; i++) {
-    console.log(i + " -- " + i); 
-
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log(i + " -- FIZZBUZZ"); 
-    } else if (i * 3 === 0) {
-        console.log(i + " -- FIZZ"); 
+        console.log(i + " -- FIZZBUZZ");
+    } else if (i % 3 === 0) {
+        console.log(i + " -- FIZZ");
     } else if (i % 5 === 0) {
-        console.log(i + " -- BUZZ"); 
+        console.log(i + " -- BUZZ");
     } else {
-        console.log(i + " -- " + i); 
+        console.log(i + " -- " + i);
     }
-
 }
