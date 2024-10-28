e726076d2e3ccc4e88a186cace7a8761f2b06a87
Cristian Teletor <ronicristianteletor@gmail.com>
2024-10-28 11:41:09 +0100

    I redid my function that generates the number from 1 to 100.

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-/*for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(i + " -- FIZZBUZZ");
-    } else if (i % 3 === 0) {
-        console.log(i + " -- FIZZ");
-    } else if (i % 5 === 0) {
-        console.log(i + " -- BUZZ");
-    } else {
-        console.log(i + " -- " + i);
-    }
-} */
 
 /*const there =  (x,y, z) => {
  
@@ -22,11 +11,23 @@ console.log(there())*/
 
 
 
-const sum = (x, y, z) => {
-    let result = (x % y) + z;
-    return result; 
+// const sum = (x, y, z) => {
+//     let result = (x % y) + z;
+//     return result; 
+// }
+
+
+// sum(15, 24, 56)
+// console.log(sum(15, 24, 56));
+
+for(let i = 0; i < 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(i, "-- FIZZBUZZ")
+    }else if(i % 3 === 0){
+        console.log(i,"-- FIZZ")
+    }else if(i % 5 === 0){
+        console.log(i,"-- BUZZ")
+    }else {
+        console.log(i + "--" + i)
+    }
 }
-
-
-sum(15, 24, 56)
-console.log(sum(15, 24, 56));
